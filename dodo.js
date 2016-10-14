28c34975c9366521c76a1064ae95682e554f3493
James Sibert <jsibert@basecampcodingacademy.org>
2016-10-14 13:58:09 -0500

    bird is movable

--- dodo.js
+++ dodo.js
@@ -1,4 +1,3 @@
-// image replace on left/right movement
 // move pipes
 // randomly generate pipes in correct position
 // collision
@@ -22,6 +21,7 @@ var game = {
     var i = this.dodo_pos;
     this.columns[i][6] = null;
     this.columns[i - 1][6] = "dodo";
+    this.dodo_pos = i - 1;
     this.draw();
   },
 
@@ -29,14 +29,42 @@ var game = {
     var i = this.dodo_pos;
     this.columns[i][6] = null;
     this.columns[i + 1][6] = "dodo";
+    this.dodo_pos = i + 1;
     this.draw();
   },
 
   draw: function () {
-    for i in columns:
-      for j in columns:
-          item = $('#tablecenter').children("div")[i]
-          kid = item.children("img")[j]
-          kid.attr("src", "img/???.png")
-}}
+    for (i = 0; i < 9; i++) {
+      for (j = 0; j < 8; j++) {
+          item = $('#tablecenter').children("div")[7 -j];
+          kid = $(item).children("img")[i];
+          $(kid).attr("src", this.image_for(this.columns[i][j]));
+      }
+    }
+  },
+
+  image_for: function(value) {
+    if (value === "dodo") {
+      return "img/dodo.png";
+    } else if (value === "pipe") {
+      return "img/pipe.png";
+    } else {
+      return "img/blue.png";
+    }
+  }
+};
+$(document).keydown(function (e) {
+    switch(e.which) {
+        case 37: // left
+        game.move_dodo_left();
+        break;
+
+        case 39: // right
+        game.move_dodo_right();
+        break;
+
+        default: return; // exit this handler for other keys
+  }
+  e.preventDefault(); // prevent the default action (scroll / move caret)
+});
 
